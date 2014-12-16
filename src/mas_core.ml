ee22aa9254d67cd0bf2846fa1c530cb76cf94470
carm <carmelo.piccione@gmail.com>
2014-12-16 10:37:12 -0500

    intermediate commit

--- src/mas_core.ml
+++ src/mas_core.ml
@@ -1,4 +1,3 @@
-
 module Reward = Mas_intf.Reward
 
 module type Action = Mas_intf.Action
@@ -6,18 +5,21 @@ module type Action = Mas_intf.Action
 module type S = Mas_intf.S
 
 
-module Make(Action:Action) (*: S with module Action = Action *) = 
-  struct
+module Make(Action:Action) (*: S with module Action = Action  *) = 
+struct
 
   (** Defines an agent and operations to the run the agent within the environment *)
-    module rec Agent : sig type 'a t = private {policy: 'a Policy.t} [@@deriving show] val policy : 'a t -> 'a Policy.t val init : 'a Policy.t -> 'a t end =
+  module rec Agent : 
+    sig 
+      type 'a t = private {policy: 'a Policy.t} [@@deriving show] 
+      val policy : 'a t -> 'a Policy.t val init : 'a Policy.t -> 'a t 
+    end =
   struct
     type 'a t = {policy: 'a Policy.t} [@@deriving show]
 
     let policy t = t.policy
 
     let init policy = {policy}
-
   end
 
   and 
@@ -36,19 +38,18 @@ module Make(Action:Action) (*: S with module Action = Action *) =
     type 'a t = {agent: 'a Agent.t; action: 'a Action.t; epoch: int} [@@deriving show]
   end
 
-  module Environment =
+  module Environment = 
   struct
-    type params = {trials:int} [@@deriving show, ord]
-    type 'a t = {params:params; agents:'a Agent.t list}
-
+    type params = {trials:int} [@@deriving show]
+    type 'a state = {params:params; agents:'a Agent.t list; obs:'a Observation.t}
+    type 'a t = 'a state Gen.t   
+    (** TODO start action / " for initial state? 
     let init params agents = {params;agents} 
 
-    let run params t = t
+    let run params t = ()
 
     let agents t = t.agents
 
     let reward a t = 0.0
   end
-   
-  
-  end
+end
