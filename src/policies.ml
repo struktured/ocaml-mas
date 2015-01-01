f0042cbe7a180376f79701d66c4ac0d83b616de4
carm <carmelo.piccione@gmail.com>
2015-01-01 00:05:38 -0500

    use arrays for actions rather than lists

--- src/policies.ml
+++ src/policies.ml
@@ -9,20 +9,20 @@ struct
 
   module Value_fn = Value_functions.Make_discrete(State)(Action)
 
-  let init ?(eps=default_eps) value_fn (action_provider : State.t -> Action.t list) : (State.t, Action.t) Agents.State_based_policy.t =
+  let init ?(eps=default_eps) value_fn (action_provider : State.t -> Action.t array) : (State.t, Action.t) Agents.State_based_policy.t =
    let rand_float = CCRandom.float 1.0 in
    let rand_int = CCRandom.int Pervasives.max_int in
    fun (state:State.t) -> 
      let actions = action_provider state in
-     let expectations = List.map (fun action -> (action, Value_fn.value value_fn ~action state)) actions in
+     let expectations = CCArray.map (fun action -> (action, Value_fn.value value_fn ~action state)) actions in
      let exploit = Random.run rand_float <= eps in
      if exploit then 
-      let (a, exp) = List.fold_right (fun ((best_action, best_exp) as best) ((cur_action, cur_exp) as cur) -> 
+      let (a, exp) = CCArray.fold (fun ((best_action, best_exp) as best) ((cur_action, cur_exp) as cur) -> 
          if (cur_exp >= best_exp) then cur else best) 
-         expectations 
-         (List.hd actions, Reward.min_value)
+         (CCArray.get actions 0, Reward.min_value)
+         expectations
       in a 
      else 
-       let index = (Random.run rand_int) mod (List.length actions) in
-       List.nth actions index
+       let gen = CCArray.random_choose actions in
+       Random.run gen
 end
