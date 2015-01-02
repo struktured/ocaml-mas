b6acc021efdac3caa76c428c6fa722bbee3b56e0
carm <carmelo.piccione@gmail.com>
2015-01-02 01:51:56 -0500

    check num actions > 0 in random policy

--- src/policies.ml
+++ src/policies.ml
@@ -23,6 +23,7 @@ module RandomPolicy =
         let open CCArray in
         let actions = action_provider s in
         let num_actions = CCArray.length actions in
+        if num_actions = 0 then failwith("no actions possible for state") else
         let weights = CCOpt.get_lazy (fun () -> fun (_:'s) (_:'a array) -> actions >>|
           fun (_:'a) -> 1.0 /. CCFloat.of_int num_actions) weights in
         let index = sample ~rand (weights s actions) in
