5205e8bfa37f023d011ca44859371e3ae4560da5
carm <carmelo.piccione@gmail.com>
2015-01-18 03:16:16 -0500

    add x and y labels

--- src/archimedes_plot.ml
+++ src/archimedes_plot.ml
@@ -14,6 +14,8 @@ let init ?(viewport=A.init ["graphics"]) () =
 let close t = A.close t.viewport
 let running_avg t g = 
   A.Viewport.title t.viewport "Average Reward Over Time";
+  A.Viewport.xlabel t.viewport "Epoch";
+  A.Viewport.ylabel t.viewport "Reward";
   A.Axes.box ~grid:true t.viewport;
   let module Cache = CCHashtbl.Make(CCInt) in
   let cache = Cache.create 0 in
