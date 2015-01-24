2c5bfd07b7fac42fb4837754e8c294f2778f0a94
carm <carmelo.piccione@gmail.com>
2015-01-24 13:01:56 -0500

    use archimedes path api to render dynamically

--- src/archimedes_plot.ml
+++ src/archimedes_plot.ml
@@ -15,17 +15,15 @@ let running_avg t g turn =
   A.Viewport.xlabel t.viewport "Epoch";
   A.Viewport.ylabel t.viewport "R";
   A.Axes.box ~grid:true t.viewport;
-  let module Cache = CCHashtbl.Make(CCInt) in
-  let cache = Cache.create 0 in
+  let p = A.Path.make() in
+  A.Path.move_to p 0.0 0.0;
   Mas_plot.running_avg g turn |> 
   Gen_ext.consume_second
     (fun (_, (cnt, avg_reward)) ->
-       Cache.add cache cnt avg_reward;
-       A.fx 
-         t.viewport 
-         (fun x -> let i = CCFloat.to_int x in CCOpt.get 0.0 (Cache.get cache i)) 
-         0.0 
-         (CCFloat.of_int cnt);
+       let x = CCFloat.of_int cnt in
+       let y = avg_reward in
+       A.Path.line_to p x y;
+       A.Viewport.stroke t.viewport `Data p;
        A.show t.viewport) 
 
 
