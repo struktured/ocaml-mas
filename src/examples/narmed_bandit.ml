54f43eab6ba01efa1ae0a5ca870e29002184fe2f
carm <carmelo.piccione@gmail.com>
2015-03-14 22:43:42 -0400

    sarsa learner complete!

--- src/examples/narmed_bandit.ml
+++ src/examples/narmed_bandit.ml
@@ -40,11 +40,11 @@ struct
   let init_opponent ?arm_rewards ?(num_arms=default_arms) () : opponent =
     let rand = CCRandom.float 1.0 in
     let open Gen.Infix in
-    let arm_rewards = CCOpt.get_lazy (fun () -> Gen.to_array (Gen.(0--(num_arms-1)) >>| 
+    let arm_rewards = CCOpt.get_lazy (fun () -> Gen.to_array (Gen.(0--(num_arms-1)) >>|
       fun (_:int) -> noisy (CCRandom.run rand))) arm_rewards in
     let policy : (Reward.t, Arm.t) Policy.t = fun obs -> match obs.action with a -> (arm_rewards.(a) ()) in
-    Agent.init policy (fun obs -> 0.0) (Value_fn.init ~count:(fun ?action obs -> 0) 
-      ~value:(fun ?action obs -> 0.0) ~update:(fun t ~action obs r -> t)) 
+    Agent.init policy (fun obs -> 0.0) (Value_fn.init ~count:(fun ?action obs -> 0)
+      ~value:(fun ?action obs -> 0.0) ~update:(fun t ~action obs r -> t))
       ~name:((string_of_int (CCArray.length arm_rewards)) ^ "-armed bandit")
 
   let init ?arm_rewards ?num_arms ~trials ~(agent:agent) : (Arm.t, Reward.t) Env.t =
@@ -52,9 +52,9 @@ struct
     let params : params = {trials;init_obs=Env.from_opponent_obs 0.0 opponent} in
     Env.init ~params ~agent ~opponent
 
-  let init_with_policy ?arm_rewards ?num_arms ~trials ?(name="player") policy value_fn = 
+  let init_with_policy ?arm_rewards ?num_arms ~trials ?(name="player") policy value_fn =
     init ?arm_rewards ?num_arms ~trials ~agent:(init_agent policy value_fn name)
-end 
+end
 
 open NArmedBandit
 module Env = Environment_2_agents
@@ -67,38 +67,40 @@ let c = 2.0
 type policy = [`Random | `Greedy | `UCT]
 type learning_rule = [`Mean_update | `Q_learner]
 module Q_learner = Learning_rules.Q_learner.Make(Value_function)
+module Sarsa_learner = Learning_rules.Sarsa_learner.Make(Value_function)
 
 let go ?(policy=`Greedy) ?(learning_rule=`Mean_update) ?(eps=eps) ?(c=c) ?weights
   ?(trials=trials) ?(arm_weights=arm_weights) ?(show_plot=true) () =
   let arm_rewards = CCArray.map (fun x -> fun () -> x) arm_weights in
-  let max_weight, max_index = 
-    CCArray.foldi (fun (w, k) i w' -> 
+  let max_weight, max_index =
+    CCArray.foldi (fun (w, k) i w' ->
       if w' > w then (w', k+1) else (w, k)) (0.0, 0) arm_weights in
   let num_arms = CCArray.length arm_rewards in
-  let action_provider _ = CCArray.(0--(num_arms-1)) in 
+  let action_provider _ = CCArray.(0--(num_arms-1)) in
   let learning_rule = match learning_rule with
     | `Mean_update -> None
+    | `Sarsa_learner -> Some (Sarsa_learner.init action_provider)
     | `Q_learner -> Some (Q_learner.init action_provider) in
   let value_function = Value_function.init "discrete" ?learning_rule in
   let policy = match policy with 
-    | `Random -> Policies.RandomPolicy.init ?weights action_provider 
-    | `Greedy -> GreedyPolicy.init ~eps value_function action_provider 
+    | `Random -> Policies.RandomPolicy.init ?weights action_provider
+    | `Greedy -> GreedyPolicy.init ~eps value_function action_provider
     | `UCT -> UCTPolicy.init ~c value_function action_provider in
   let env = NArmedBandit.init_with_policy
               ~arm_rewards ~trials policy value_function in
   let plot = if show_plot then Some (Archimedes_plot.init ()) else None in
   last_plot := plot;
-  let player_turn = Env.Agent in 
-  let env = CCOpt.maybe (fun p -> Archimedes_plot.running_avg p 
+  let player_turn = Env.Agent in
+  let env = CCOpt.maybe (fun p -> Archimedes_plot.running_avg p
     ~turn:player_turn ~ub:(fun s -> max_weight) env) env plot in
   let print (s, avg) =
     let reward = Env.reward s Env.Agent in
     print_endline ("State: " ^ Environment_2_agents.show_state Arm.pp Reward.pp s);
     print_endline (
-      "Epoch: " ^ string_of_int avg.R.agent_epoch ^ 
+      "Epoch: " ^ string_of_int avg.R.agent_epoch ^
       ", Reward: " ^ Reward.show reward ^
       ", Avg Reward: " ^ Reward.show avg.R.agent_avg
-    ) in 
+    ) in
   let env = R.decorate env in
   Gen.iter print env;plot
 
