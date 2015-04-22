4e77561485621ed31a27e9af5948936a860e1ab7
cpiccion <carmelo.piccione@theice.com>
2015-04-22 12:35:23 -0400

    more agent pipe work

--- src/environments/async/agent_pipe.ml
+++ src/environments/async/agent_pipe.ml
@@ -3,30 +3,46 @@ open Async.Std
 open Mas_core_async
 open Mas_async_system
 
-module Bidirectional = struct
-type ('a, 'b) obs = 
-  | From_agent of ('b, 'a) Observation.t 
-  | To_agent of ('a,'b) Observation.t (** [@@deriving show] *)
+module type S = sig
 
-let create (from_agent:('b, 'a) Agent.t)
-    (to_agent : ('a, 'b) Agent.t) (init_obs:('a,'b) obs) 
-     (*:('a, 'b) Agent.t * ('b, 'a) Agent.t Deferred.t *) =
-    let reader1, writer1 = Pipe.create () in
-    let reader2, writer2 = Pipe.create () in
-    let open Deferred.Monad_infix in
-    let read_write r w agent : [`Repeat of ('c,'d) Agent.t | `Finished of
-                                  ('d,'c) Agent.t] Deferred.t
-      = Pipe.read r >>|
+type ('a, 'b) obs =
+  | From_agent of ('b, 'a) Observation.t
+  | From_opponent of ('a, 'b) Observation.t  (* [@@deriving show] *)
+val create : ('a, 'b) Agent.t -> ('b, 'a) Agent.t -> ('a,'b) obs 
+    -> ('a, 'b) Agent.t * ('b, 'a) Agent.t Deferred.t
+ 
+end
+
+
+module Bidirectional : S = struct
+
+type ('a, 'b) obs =
+  | From_agent of ('b, 'a) Observation.t
+  | From_opponent of ('a, 'b) Observation.t  (* [@@deriving show] *)
+
+let read_write r w agent = 
+      let open Deferred.Monad_infix in 
+      Pipe.read r >>=
         function
-          | `Eof -> `Finished agent
-          | `Ok obs -> `Repeat agent 
-(*            (let policy = Agent.policy agent in policy obs >>=
-              fun obs' -> Pipe.write w obs' >>| fun () -> `Repeat agent) *)
-    in
+          | `Eof -> Deferred.return(`Finished agent)
+          | `Ok obs ->
+            (let policy = Agent.policy agent in policy obs >>=
+              fun obs' -> Pipe.write w obs' >>| fun () -> `Repeat agent)
+
+let create (from_agent:('a, 'b) Agent.t)
+    (from_opp  : ('b, 'a) Agent.t) (init_obs:('a,'b) obs) =
+    let (reader1 : ('b, 'a) Observation.t Pipe.Reader.t),
+        (writer1 : ('b, 'a) Observation.t Pipe.Writer.t) = Pipe.create () in
+    let (reader2 : ('a, 'b) Observation.t Pipe.Reader.t),
+        (writer2 : ('a, 'b) Observation.t Pipe.Writer.t) = Pipe.create () in
+    let open Deferred.Monad_infix in 
     (match init_obs with
-      | From_agent from_obs -> Pipe.write writer2 from_obs
-      | To_agent to_obs -> Pipe.write writer1 to_obs)
-    (*>>= (fun () -> Deferred.both
-      (Deferred.repeat_until_finished from_agent (read_write reader1 writer2))
-      (Deferred.repeat_until_finished to_agent (read_write reader2 writer1))) *)
+      | From_agent from_obs -> Pipe.write writer1 from_obs
+      | From_opponent to_obs -> Pipe.write writer2 to_obs)
+    >>= fun () ->
+    let f1 = read_write reader1 writer2 in
+    let f2 = read_write reader2 writer1 in
+    Deferred.both
+      (Deferred.repeat_until_finished from_agent f1)
+      (Deferred.repeat_until_finished from_opp f2)
 end
