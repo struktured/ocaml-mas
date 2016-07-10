c91f5b7871926c80ff54de35d81e91dae466366a
Carmelo Piccione <carmelo.piccione@gmail.com>
2016-07-10 14:36:23 -0400

    wip

--- src/core/sampling_ext.ml
+++ src/core/sampling_ext.ml
@@ -3,4 +3,4 @@ module Random = CCRandom
 let from_weights ?rand weights =
   let sum = CCArray.fold (+.) 0. weights in
   let normalized = CCArray.map (fun w -> w /. sum) weights in
-  Oml.Sampling.multinomial normalized
+  Oml.Statistics.Sampling.multinomial normalized
--- src/value_functions/discrete_value_function.ml
+++ src/value_functions/discrete_value_function.ml
@@ -35,30 +35,40 @@ module StateAction = struct
     end
 end
 
-
-module Make_with_cache (StateAction : StateAction.S) (Cache:
-  Prob_cache_containers.Model_intf.S with module Event := StateAction) :
-  S with module State = StateAction.State and module Action = StateAction.Action =
+module State_action_update_fn =
 struct
-  include Value_function.Make(StateAction.State)(StateAction.Action)
-  
-  module StateAction = StateAction
+  module type S = sig
+    module Obs : StateAction.S
+    include Prob_cache_common.Update_rules.S with module Obs := Obs
+  end
+end
 
-  type update_rule = StateAction.t Prob_cache_common.Update_rules.Update_fn.t
+module Make_with_cache (State_action_update_fn : State_action_update_fn.S) (Cache:
+  Prob_cache_containers.Model_intf.S with module Event := State_action_update_fn.Obs) :
+  S with
+    module State = State_action_update_fn.Obs.State and
+    module Action = State_action_update_fn.Obs.Action =
+struct
+  module Obs = State_action_update_fn.Obs
+  include Value_function.Make(Obs.State)(Obs.Action)
+  module Update_rules = Prob_cache_common.Update_rules
+  module Update_rule = Update_rules.Rule_wrap(State_action_update_fn)
+  module Mean_update_rule = Update_rules.Mean(Update_rule.Obs)
+  type update_rule = Update_rules.UPDATE_FN(Obs).t
 
   let events_of ?action s = Cache.Events.of_list (
     match action with
-    | Some a -> [StateAction.State s; StateAction.Action a]
-    | None -> [StateAction.State s])
+    | Some a -> [Obs.State s; Obs.Action a]
+    | None -> [Obs.State s])
 
   let state_of events : State.t option = let event_list = Cache.Events.to_list events in
-    let rec f_rec = function [] -> None | (StateAction.State s)::l -> Some s | (_::l) -> f_rec l in
+    let rec f_rec = function [] -> None | (Obs.State s)::l -> Some s | (_::l) -> f_rec l in
     f_rec event_list
 
   let state_of_exn events = CCOpt.get_exn (state_of events)
 
   let action_of events : Action.t option = let event_list = Cache.Events.to_list events in
-    let rec f_rec = function [] -> None | (StateAction.Action a)::l -> Some a | (_::l) -> f_rec l in
+    let rec f_rec = function [] -> None | (Obs.Action a)::l -> Some a | (_::l) -> f_rec l in
     f_rec event_list
 
   let action_of_exn events = CCOpt.get_exn (action_of events)
@@ -85,11 +95,11 @@ struct
     let prior_exp = CCOpt.map (fun pr -> (fun events ->
       with_s_a events pr 0.0)) prior_reward in
     let vf = ref None in
-    let update_rule = CCOpt.map (fun rule -> fun ?orig ~obs ~exp ~cnt ->
+    let update_rule : Cache.update_rule option = CCOpt.map (fun rule -> fun ?orig ~obs ~exp ~cnt ->
       (* Only specialize the update rule when it's a state action pair
          that is being updated *)
       if action_of obs = None || state_of obs = None
-        then Prob_cache_common.Update_rules.mean ?orig ~obs ~exp ~cnt
+        then (*Mean_update_rule.apply ?orig ~obs ~exp ~size:cnt *) failwith("mean_update_rule.apply")
       else
           let t', r' = rule (CCOpt.get_exn !vf, CCOpt.get 0.0 orig)
             (action_of_exn obs) (state_of_exn obs) exp in r') learning_rule in
@@ -107,7 +117,7 @@ end
 (** Creates a discrete state value function- it maps discrete state and actions with
     reward estimates by caching them explicitly. These are also typically classified
     as tabular methods in the reinforcement learning literature. *)
-module Make(StateAction:StateAction.S) = Make_with_cache 
- (StateAction) (Prob_cache_containers.Set_model.Make(StateAction))
+module Make(State_action_update_fn:State_action_update_fn.S) = Make_with_cache 
+ (State_action_update_fn)(Prob_cache_containers.Set_model.Make(StateAction))
 
 
